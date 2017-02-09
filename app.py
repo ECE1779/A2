838e08db7fb235edbbe9d85e5ac080878ce9eb95
Wenchang Zhang <npsfsz@gmail.com>
2017-02-09 15:00:55 -0800

    change

--- app.py
+++ app.py
@@ -37,13 +37,14 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-                    if message_text == "trump":
-                        send_message(sender_id, "make america great again")
-                    elif message_text == "hello":
-                        send_message(sender_id, "world")
-                    else:
-                        send_message(sender_id, "got it")
+                    # message_text = messaging_event["message"]["text"]  # the message's text
+                    # if message_text == "trump":
+                      #  send_message(sender_id, "make america great again")
+                    # elif message_text == "hello":
+                      #  send_message(sender_id, "world")
+                    # else:
+                      #  send_message(sender_id, "got it")
+                    send_message(sender_id, "got it")
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
 
