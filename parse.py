968571cbf888c5f49f76de12533563f5f5cd4bc3
npsfsz <npsfsz@gmail.com>
2017-03-02 17:21:51 -0500

    qweqweqwe

--- parse.py
+++ parse.py
@@ -97,4 +97,60 @@ def send_back(sender_id, message_text):
 
 
 
+def send_image(recipient_id, contentUrl):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=contentUrl))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message":{
+            "attachment":{
+            "type":"image",
+            "payload":{
+            "url":contentUrl
+      }
+    }
+  }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+
+
 
