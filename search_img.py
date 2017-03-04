0397ab600fcf149875d4d93f490e467d9856c305
Wenchang Zhang <npsfsz@gmail.com>
2017-03-03 23:52:09 -0500

    abc

--- search_img.py
+++ search_img.py
@@ -74,10 +74,10 @@ def search_image_3(q, offset):
     except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
-    json_data = json.loads(data)
+    #json_data = json.loads(data)
     print(json_data)
 
-    if json_data["value"]["0"] is None:
+    if json_data["value"][0] is None:
         return None
     else:
         return json_data["value"][0]["contentUrl"]
